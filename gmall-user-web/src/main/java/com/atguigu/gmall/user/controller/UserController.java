7160acc1e02f10bfca8f617d0070f21343c96889
dzz <16245603072@qq.com>
2020-02-04 16:48:39 +0800

    day03 commit gmail

--- gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.atguigu.gmall.user.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.UmsMember;
 import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.service.UserService;
@@ -8,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.alibaba.dubbo.config.annotation.Reference;
 import java.util.List;
 
 @Controller
